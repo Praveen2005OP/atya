e000c56ef8b3411982687afa92f9585bda50b934
Praveen Kumar Sharma <sharmakarpraveen@gmail.com>
2026-06-07 18:49:12 +0530

    Added new feature

--- atya/settings.py
+++ atya/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,6 +125,14 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = (
+    "whitenoise.storage.CompressedManifestStaticFilesStorage"
+)
+
+ALLOWED_HOSTS = ["*"]
 
 # To send feedback via email (configure these settings in production)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'  # For development, prints emails to console
