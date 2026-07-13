70c7c545653487be80d83cdb1b946bff948e1a40
Praveen Kumar Sharma <sharmakarpraveen@gmail.com>
2026-07-13 22:18:33 +0530

    update

--- atya/settings.py
+++ atya/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+import dj_database_url
 
 load_dotenv()
 
@@ -77,11 +78,18 @@ WSGI_APPLICATION = 'atya.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# ----------------------------------------------------
+# Database
+# Local  -> SQLite
+# Render -> PostgreSQL (DATABASE_URL)
+# ----------------------------------------------------
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
